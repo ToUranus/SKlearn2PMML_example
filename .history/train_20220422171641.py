c44ee13590445b1c610e4126553775703a1553f5
ByUranus <loveyu_h@sina.com>
2022-04-22 17:26:47 +0800

    An simple PMML example

---
+++ .history/train_20220422171641.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description:       :
+@Date     :2022/04/22 16:25:14
+@Author      :ShiFei
+@version      :1.0
+'''
+
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingClassifier
+
+# data
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Training and Testing split
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=10)
+
+# pipeline:must use PMMLpipeline
+from sklearn2pmml.pipeline import PMMLPipeline
+
+classfier = GradientBoostingClassifier(random_state=10)
+pipe = PMMLPipeline([('Standar',StandardScaler()),('Classfier',classfier)])
+
+# fit
+pipe.fit(x_train,y_train)
+
+# evalute
+print(pipe.score(x_test,y_test))
+
+# converting pipelines to PMML:you must have a lib named sklearn2pmml,
+# github:https://github.com/jpmml/sklearn2pmml
+# use command: pip install sklearn2pmml if sklearn2pmml not exist
+
+# save pmml
+from sklearn2pmml import sklearn2pmml
+sklearn2pmml(pipe,'gbdt.pmml',with_repr=True)
+
+
